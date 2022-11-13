8b17f2e0ff9bdbb413e61de4b459d93159152ad7
rc-coder <rcrd.campelo@gmail.com>
2022-11-13 12:32:54 -0300

    Pequeño arreglo de placeholder

---
+++ prueba.js
@@ -0,0 +1,9 @@
+let arrayOfNum = [1, 2, 3, 4, 5];
+
+let mayorValor = Math.min(...arrayOfNum);
+
+console.log(mayorValor);
+
+const brand = document.getElementById('brand');
+
+brand.innerHTML = 'Mi portfolio';
